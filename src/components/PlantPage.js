022ec656345579d75ac9fb84282d5ea315d0629d
Hallie Sala <halliesala19@gmail.com>
2023-08-09 10:42:25 -0400

    code challenge core deliverables

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+      fetch('http://localhost:6001/plants')
+      .then(data => data.json())
+      .then(data => {
+        setPlants(data);
+      })
+    }
+    ,
+    []
+  )
+
+  function addPlant(plant) {
+    setPlants([...plants, plant]);
+  }
+
+  const filteredPlants = plants.filter(plant => {
+    return (
+      plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  })
+
+  
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant} />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
